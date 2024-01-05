3753b474d55f88dc7756d6b942e9aa48ca6fa2e0
poprigun4ik99 <oleksii.kondrashov.workmail@gmail.com>
2024-01-05 05:46:37 +0200

    Wip module 1

---
+++ app/src/main/java/com/testarchitecture/dynamicfeatureapp/SharedDataObject.kt
@@ -0,0 +1,5 @@
+package com.testarchitecture.dynamicfeatureapp
+
+object SharedDataObject {
+    val someSharedData1 = "someSharedData from app module"
+}
--- dynamicfeature1/src/main/java/com/testarchitecture/dynamicfeature1/Feature1Activity.kt
+++ dynamicfeature1/src/main/java/com/testarchitecture/dynamicfeature1/Feature1Activity.kt
@@ -2,10 +2,16 @@ package com.testarchitecture.dynamicfeature1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.testarchitecture.dynamicfeatureapp.SharedDataObject
 
 class Feature1Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_feature1)
+
+        findViewById<TextView>(R.id.tv_shared_data).apply {
+            setText(SharedDataObject.someSharedData1)
+        }
     }
 }
