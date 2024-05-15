1101c08b7b3975667cac845b48ae4ac2e91b99ac
poprigun4ik99 <oleksii.kondrashov.workmail@gmail.com>
2024-05-15 12:55:10 +0300

    update dependency

--- feature1/data/build.gradle.kts
+++ feature1/data/build.gradle.kts
@@ -67,5 +67,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation("com.google.android.play:core:1.10.3")
+    implementation("com.google.android.play:feature-delivery-ktx:2.1.0")
 }
